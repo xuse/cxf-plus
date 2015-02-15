c2396340316be7ba85d8662f7736877144875868
xuse <hzjiyi@gmail.com>
2015-02-15 17:04:29 +0800

    Adding java-doc.

--- src/main/java/org/easyframe/cxfplus/support/ServiceDefinition.java
+++ src/main/java/org/easyframe/cxfplus/support/ServiceDefinition.java
@@ -14,29 +14,62 @@ public class ServiceDefinition {
 	private Object serviceBean;
 	private String path;
 	
+	/**
+	 * 返回服务名。服务名同时也是服务的HTTP URL的一部分。比如
+	 * 服务名为 fooService的 服务，发布到 http://host:port/context/ws/fooService的URL上。
+	 * @return 服务名
+	 */
 	public String getName() {
 		if(name==null)return serviceClass.getName();
 		return name;
 	}
 
+	/**
+	 * 设置服务名
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * 构造
+	 * @param name 服务名
+	 * @param sei  服务接口
+	 * @param serviceInstance 服务实现对象
+	 */
 	public ServiceDefinition(String name,Class<?> sei,Object serviceInstance) {
 		this.name=name;
 		this.serviceClass = sei;
 		this.serviceBean = serviceInstance;
 	}
 
+	/**
+	 * 空构造
+	 */
+	public ServiceDefinition() {
+	}
+
+	/**
+	 * 获得服务接口类
+	 * @return 服务接口类
+	 */
 	public Class<?> getServiceClass() {
 		return this.serviceClass;
 	}
 
+	/**
+	 * 获得服务实现对象
+	 * @return 服务实现对象
+	 */
 	public Object getServiceBean() {
 		return this.serviceBean;
 	}
 
+	/**
+	 * 获得服务的路径
+	 * @return 服务的路径
+	 */
 	public String getPath() {
 		if(path==null){
 			 return "/" + getName();
@@ -44,9 +77,11 @@ public class ServiceDefinition {
 		return path;
 	}
 
+	/**
+	 * 设置服务路径，服务路径为null时，即使用/+服务名 
+	 * @param path
+	 */
 	public void setPath(String path) {
 		this.path = path;
 	}
-	
-	
 }
